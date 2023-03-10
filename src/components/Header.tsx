a9761b1d700331a25a6a5a20c262517ad797a19e
vipulsoni <vipul.soni@tigeranalytics.com>
2023-03-10 16:23:20 +0530

    Subscribed Trips Slideover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,37 @@
-import React from "react";
-import StarIcon from '@mui/icons-material/Star';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import HelpIcon from '@mui/icons-material/Help';
+import React, { useState } from 'react'
+import StarIcon from '@mui/icons-material/Star'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import HelpIcon from '@mui/icons-material/Help'
+import SlideOver from './SlideOver'
+import SubscribedTrips from './SubscribedTrips'
 
 function Header() {
-  return (
-    <div className="bg-white px-4 py-3 flex items-center w-full">
-      <div className="flex items-center w-[50%]">
-        <div className="text-3xl text-themeBlue3">SENSE</div>
-      </div>
-      <div className="flex items-center w-[50%] justify-end px-3 pr-5">
-        <div className="float-right flex gap-4">
-          <div className="cursor-pointer">
-            <StarIcon sx={{ color: "#003668" }} />
-          </div>
-          <div className="cursor-pointer">
-            <NotificationsIcon sx={{ color: "#003668" }} />
-          </div>
-          <div className="cursor-pointer">
-            <HelpIcon sx={{ color: "#003668" }} />
-          </div>
-        </div>
-      </div>
+ const [showSlider, setShowSlider] = useState<boolean>(false)
+ return (
+  <div className='bg-white px-4 py-3 flex items-center w-full'>
+   <div className='flex items-center w-[50%]'>
+    <div className='text-3xl text-themeBlue3'>SENSE</div>
+   </div>
+   <div className='flex items-center w-[50%] justify-end px-3 pr-5'>
+    <div className='float-right flex gap-4'>
+     <div className='cursor-pointer'>
+      <StarIcon sx={{ color: '#003668' }} onClick={() => setShowSlider(true)} />
+      {showSlider && (
+       <SlideOver closeOnClick={() => setShowSlider(false)}>
+        <SubscribedTrips />
+       </SlideOver>
+      )}
+     </div>
+     <div className='cursor-pointer'>
+      <NotificationsIcon sx={{ color: '#003668' }} />
+     </div>
+     <div className='cursor-pointer'>
+      <HelpIcon sx={{ color: '#003668' }} />
+     </div>
     </div>
-  )
-};
+   </div>
+  </div>
+ )
+}
 
-export default Header;
+export default Header
